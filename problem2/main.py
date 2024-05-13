275bcf875c0a375e082cf42e2b0ef75737091eb2
masgal <dimasrumekso28@gmail.com>
2024-05-13 17:28:34 +0700

    solution2

--- problem2/main.py
+++ problem2/main.py
@@ -1,9 +1,18 @@
 def caesar(offset, input_str):
-    return ''
+    encrypted_str = ''
+    for char in input_str:
+        if char.isalpha():
+            shifted = ord(char) + offset
+            if char.islower():
+                shifted = (shifted - ord('a')) % 26 + ord('a')
+            encrypted_str += chr(shifted)
+        else:
+            encrypted_str += char
+    return encrypted_str
 
 if __name__ == '__main__':
-    print(caesar(3, "abc")) # def
-    print(caesar(2, "alta")) # cnvc
-    print(caesar(10, "alterraacademy")) # kvdobbkkmknowi
-    print(caesar(1, "abcdefghijklmnopqrstuvwxyz")) # bcdefghijklmnopqrstuvwxyza
-    print(caesar(1000, "abcdefghijklmnopqrstuvwxyz")) # mnopqrstuvwxyzabcdefghijkl
+    print(caesar(3, "abc"))  # def
+    print(caesar(2, "alta"))  # cnvc
+    print(caesar(10, "alterraacademy"))  # kvdobbkkmknowi
+    print(caesar(1, "abcdefghijklmnopqrstuvwxyz"))  # bcdefghijklmnopqrstuvwxyza
+    print(caesar(1000, "abcdefghijklmnopqrstuvwxyz"))  # mnopqrstuvwxyzabcdefghijkl
